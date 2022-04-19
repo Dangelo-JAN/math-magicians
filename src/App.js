2a0cc6da77c34835cb27e34258b6b38f0ec7fd30
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-19 19:46:17 -0400

    Update de state in the calculator.js file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
