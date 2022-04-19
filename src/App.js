9ed75838f302b6985ae0f79d9742ac3005975db2
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-19 11:55:15 -0400

    1st. update after changes required

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React, { Component } from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
