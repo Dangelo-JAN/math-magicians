1de8cc1a7f204f1539f6980b079f2237ee96a690
buhariaminat <buhariaminat@yahoo.com>
2022-04-28 19:09:30 +0100

    Unit test for calculator

--- src/App.test.js
+++ src/App.test.js
@@ -30,6 +30,40 @@ describe('My Components logic it', () => {
 
     it('Residual test', () => {
       expect(Operate(1, 1, '%')).toBe('0');
+    }); 
+  });
+  
+  describe('Calculate test', () => {
+    it('AC test', () => {
+      expect(Calculate({ total: 9, next: 6, operation: '+', }, 'AC'))
+      .toEqual({ total: null, next: null, operation: null, });
+    });
+
+    it('Total and Next are provided test', () => {
+      expect(Calculate({ total: '3', next: '4', operation: '+' }, '9'))
+      .toEqual({ total: '3', next: '49', operation: '+' });
+    });
+    
+    it('Total and Next are empty test', () => {
+      expect(Calculate({ total: null, next: null, operation: '+' }, '9'))
+      .not.toEqual({ total: '9', next: null, operation: '+' });
+    });
+    
+    it('+/- button test', () => {
+      expect(Calculate({ total: '1', next: '2', operation: '+' }, '+/-'))
+      .toEqual({ total: '1', next: '-2', operation: '+' });
+    });
+    
+    it('. button test', () => {
+      expect(Calculate({ total: '1', next: '2', operation: '+' }, '.'))
+      .toEqual({ total: '1', next: '2.', operation: '+' });
+    });
+
+    it('= button test', () => {
+      const calculation = Calculate({ total: '1', next: '2', operation: '+' }, '=');
+      expect(calculation.total).toBe('3');
     });
   });
 });
+
+
