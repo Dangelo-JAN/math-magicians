34c97d8c2dc73d9922607b8309a6507193450066
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-04-28 09:37:47 -0400

    Test operations from operate test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import Operate from './logic/operate.js';
+import Calculate from './logic/calculate.js';
+
+describe('My Components logic test', () => {
+  describe('Operate test', () => {
+    it('Sum test', () => {
+      expect(Operate(1, 1, '+')).toBe('2');
+    });
+
+    it('Rest test', () => {
+      expect(Operate(1, 1, '-')).toBe('0');
+    });
+    
+    it('Multiply test', () => {
+      expect(Operate(1, 1, 'x')).toBe('1');
+    });
+
+    it('Devide test', () => {
+      expect(Operate(1, 1, '÷')).toBe('1');
+    });
+  });
+
+  // it('Calculate test', () => {
+
+  // });
 });
